adcfb15bc3aa12a72f94374c99093e8e4c31e43d
Vegars <vegars@studix.com>
2021-06-01 08:11:15 +0200

    add test for is_today when it is true

--- rikstoto-data-collector/api/raceday/tests/test_schedulable.py
+++ rikstoto-data-collector/api/raceday/tests/test_schedulable.py
@@ -1,10 +1,18 @@
 import unittest
 from api.raceday.schedulable import Schedulable
+from datetime import date
 
 class TestSchedulable(unittest.TestCase):
     def setUp(self):
         self.start_time = "2021-05-30T13:00:00"
         self.schedulable = Schedulable(self.start_time)
 
-    def test_is_today(self):
+    def test_is_today_when_false(self):
         self.assertFalse(self.schedulable.is_today)
+
+    def test_is_today_when_true(self):
+        today = date.today()
+        today_start_time = f"{today.year}-{today.month}-{today.day}T12:00:00"
+        today_schedulable = Schedulable(today_start_time)
+
+        self.assertTrue(today_schedulable.is_today)
