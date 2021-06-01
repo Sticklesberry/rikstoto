4604af98932ca1334944c5f0ec542b2fc144c1a4
Vegars <vegars@studix.com>
2021-06-02 01:18:00 +0200

    add RunnableInterface

--- rikstoto-data-collector/components/runnable.py
+++ rikstoto-data-collector/components/runnable.py
@@ -1,6 +1,12 @@
 from typing import Callable
+from abc import ABC, abstractmethod
 
-class Runnable:
+class RunnableInterface(ABC):
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
+
+class Runnable(RunnableInterface):
     def __init__(self, function: Callable, *args, **kwargs):
         self.callback = function
 
