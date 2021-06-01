35f095673b63e2f1eabed944bbb8989f8cf4cb11
Vegars <vegars@studix.com>
2021-06-01 07:59:53 +0200

    store the original rikstoto representation of timestamp in a Schedulable

--- rikstoto-data-collector/api/raceday/schedulable.py
+++ rikstoto-data-collector/api/raceday/schedulable.py
@@ -14,7 +14,12 @@ class SchedulableInterface(ABC):
 class Schedulable(SchedulableInterface):
     def __init__(self, start_time: str, *args: Any, **kwargs: Any):
         self.start_time: datetime = convert_timestamp_to_datetime(start_time)
+        self.rikstoto_time_string: str = start_time
 
     @property
     def is_today(self) -> bool:
         return self.start_time.date() == self.start_time.today().date()
+
+    @property
+    def toString(self) -> str:
+        return self.rikstoto_time_string
