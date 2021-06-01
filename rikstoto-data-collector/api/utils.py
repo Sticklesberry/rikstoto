e148f932f3e3c56faa3064b877c5977746aae5d3
Vegars <vegars@studix.com>
2021-06-01 08:03:45 +0200

    add a docstring

--- rikstoto-data-collector/api/utils.py
+++ rikstoto-data-collector/api/utils.py
@@ -2,4 +2,7 @@ from datetime import datetime
 from api.constants import timestamp_format
 
 def convert_timestamp_to_datetime(timestamp: str) -> datetime:
+    """
+    Convert a rikstoto timestamp to a python datetime object
+    """
     return datetime.strptime(timestamp, timestamp_format)
