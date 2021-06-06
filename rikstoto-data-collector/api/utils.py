e8914ba45c895cc960004666835384579ae4aa9d
Vegars <vegars@studix.com>
2021-06-06 13:22:09 +0200

    utility function that returns whatever is in the result attribute of a get request's response

--- rikstoto-data-collector/api/utils.py
+++ rikstoto-data-collector/api/utils.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 from api.constants import timestamp_format
+import requests
 
 def convert_timestamp_to_datetime(timestamp: str) -> datetime:
     """
     Convert a rikstoto timestamp to a python datetime object
     """
     return datetime.strptime(timestamp, timestamp_format)
+
+def result_of_get_request(url):
+    return requests.get(url).json()["result"]
